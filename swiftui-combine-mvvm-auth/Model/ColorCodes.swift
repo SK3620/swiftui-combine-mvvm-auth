0a18732c030a10df0633323d277d513e44cf06da
SK3620 <k.n.t11193620@gmail.com>
2024-08-06 17:10:01 +0900

    Add: ColorCodesモデル追加

---
+++ swiftui-combine-mvvm-auth/Model/ColorCodes.swift
@@ -0,0 +1,14 @@
+//
+//  ColorCodes.swift
+//  swiftui-combine-mvvm-auth
+//
+//  Created by 鈴木 健太 on 2024/08/06.
+//
+
+import Foundation
+
+enum ColorCodes {
+    case primary
+    case success
+    case failure
+}
