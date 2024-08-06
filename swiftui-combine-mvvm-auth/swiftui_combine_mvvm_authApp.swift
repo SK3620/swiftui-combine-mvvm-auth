8ca3994e272550ba977723549617edc057b4860b
SK3620 <k.n.t11193620@gmail.com>
2024-08-06 16:59:55 +0900

    Initial Commit

---
+++ swiftui-combine-mvvm-auth/swiftui_combine_mvvm_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_combine_mvvm_authApp.swift
+//  swiftui-combine-mvvm-auth
+//
+//  Created by 鈴木 健太 on 2024/08/06.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_combine_mvvm_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
